a4c22ec22510d796afc0cfe8419959150d43f67a
The Mini John <admin@phoenix-prime.gr>
2013-12-14 04:26:45 +0100

    Initial Commit - Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cruft::Application.config.secret_key_base = '92c7058a54c65f23fe2cc921dc0eedce26b5dddd3b5cdfe435a46c1f84c63a0b0efaca84d807fc6a1dc94a301951364a05b07678ae32752e9bee46a14f54ea11'
